39e58b21a0679008dabcedcd11d092b1c4d89dd8
riveraky <riverakyla_bsit@gmail.com>
2025-03-09 19:56:40 +0800

    Submitting OBJPROG Week 05 - Laboratory # 17

---
+++ GradeCalculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class GradingScale {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your score: ");
+        int score = scanner.nextInt();
+
+        char grade;
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else if (score >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("Your Grade is: " + grade);
+
+        scanner.close(); // 
+    }
+}
